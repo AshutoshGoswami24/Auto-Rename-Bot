c5ac8294d5da6e1b46dbd665f4832b2f46d13c2b
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-04-22 00:21:21 +0530

    Update route.py

--- route.py
+++ route.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Madflix_Bots")
+    return web.json_response("IAMASHU")
 
 
 async def web_server():
