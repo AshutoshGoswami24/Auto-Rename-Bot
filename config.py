119187e3b5ec4f37d27b4dabfc010b7d522c6bed
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-06-20 12:40:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ class Config(object):
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    PORT = int(os.environ.get("PORT", ""))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
