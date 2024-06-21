3bc27eb5a88548f2b55d941ee155d0057fbf3745
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-06-22 02:39:31 +0530

    Create PandaXAshu.py

---
+++ PandaXAshu.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from pytz import timezone
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from config import Config
+from aiohttp import web
+from route import web_server
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            name="renamer",
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=15,
+        )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.mention = me.mention
+        self.username = me.username  
+        self.uptime = Config.BOT_UPTIME     
+        if Config.WEBHOOK:
+            app = web.AppRunner(await web_server())
+            await app.setup()       
+            await web.TCPSite(app, "0.0.0.0",Config.PORT).start()     
+        print(f"{me.first_name} Is Started.....✨️")
+        for id in Config.ADMIN:
+            try: await self.send_message(Config.LOG_CHANNEL, f"**{me.first_name}  Is Started.....✨️**")                                
+            except: pass
+        if Config.LOG_CHANNEL:
+            try:
+                curr = datetime.now(timezone("Asia/Kolkata"))
+                date = curr.strftime('%d %B, %Y')
+                time = curr.strftime('%I:%M:%S %p')
+                await self.send_message(Config.LOG_CHANNEL, f"**{me.mention} Is Restarted !!**\n\n📅 Date : `{date}`\n⏰ Time : `{time}`\n🌐 Timezone : `Asia/Kolkata`\n\n🉐 Version : `v{__version__} (Layer {layer})`</b>")                                
+            except:
+                print("Please Make This Is Admin In Your Log Channel")
+
+Bot().run()
+
+
+
+# PandaWep
+# Don't Remove Credit 🥺
+# Telegram Channel @PandaWep
+# Developer https://github.com/PandaWep
