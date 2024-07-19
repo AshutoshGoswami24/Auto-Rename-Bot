5a05c4b52127cfa66e68d03f59673cb9d16bd42c
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-07-19 08:33:37 +0530

    Update route.py

--- route.py
+++ route.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("IAMASHU")
+    return web.json_response("https://github.com/AshutoshGoswami24/Auto-Rename-Bot")
 
 
 async def web_server():
