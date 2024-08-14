7945355f904ed2ce1382cb500821db4b98a3feb2
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-08-15 01:01:45 +0530

    😁 NOTHING

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -131,7 +131,7 @@ def extract_episode_number(filename):
     return None
 
 # Example Usage:
-filename = "Naruto Shippuden S01 - EP07 - 1080p [Dual Audio] @Madflix_Bots.mkv"
+filename = "Naruto Shippuden S01 - EP07 - 1080p [Dual Audio] @AshutoshGoswami24.mkv"
 episode_number = extract_episode_number(filename)
 print(f"Extracted Episode Number: {episode_number}")
 
