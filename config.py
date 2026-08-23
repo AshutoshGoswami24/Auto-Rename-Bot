752dc6c691b35cf2cb15e52bf520e23e6e81c6a0
funnytamil28-cmyk <funnytamil28@gmail.com>
2026-08-23 23:14:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     API_ID    = os.environ.get("API_ID", "")
     API_HASH  = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","AshutoshGoswami24")     
